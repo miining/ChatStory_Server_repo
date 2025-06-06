aeaf58c602fb98a44d771369f1ba20939713ddaa
miining <purione1@naver.com>
2025-06-06 17:26:04 +0900

    test deployment

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = MongoClient(os.getenv("MONGO_URL"))
 DB_NAME = os.getenv("DB_NAME", "chatstory")
 
 client = MongoClient(MONGO_URL)
--- app/redis/queue.py
+++
@@ -1,8 +0,0 @@
-import os
-import redis
-from rq import Queue
-
-redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-conn = redis.Redis.from_url(redis_url)
-
-queue = Queue(connection=conn)
